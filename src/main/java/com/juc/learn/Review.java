a8205aba12c49ae24d1e941d7e87de48f2b98240
liang <xiaoleliang045@gmail.com>
2021-11-30 22:46:33 +0900

    jvm learning

---
+++ src/main/java/com/juc/learn/Review.java
@@ -0,0 +1,9 @@
+package com.juc.learn;
+
+public class Review {
+    public static void main(String[] args) {
+        String info = "点赞";
+        int a = 6666;
+        final int b = 666;
+    }
+}
